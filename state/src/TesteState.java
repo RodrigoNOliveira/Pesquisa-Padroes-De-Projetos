e19f684edfc0089e87c7105753fc2d961a4058f6
Rodrigo Nathan Oliveira <rodrigonathan4101@gmail.com>
2022-11-07 00:20:25 -0300

    pesquisa padroes de projetos

---
+++ state/src/TesteState.java
@@ -0,0 +1,14 @@
+import classes.Dia;
+
+public class TesteState {
+    public static void main(String[] args) throws Exception {
+        Dia dia = new Dia();
+
+        dia.amanhecer();
+        dia.anoitecer();
+        dia.amanhecer();
+        dia.atardecer();
+        dia.atardecer();
+        
+    }
+}
